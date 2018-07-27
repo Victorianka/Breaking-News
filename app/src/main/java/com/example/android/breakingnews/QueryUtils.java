7d7373878f463d620d8e629a0cb867e4a2c73800
Anna <victorianka@gmail.com>
2018-07-27 21:23:50 +0100

    correction reversed

--- app/src/main/java/com/example/android/breakingnews/QueryUtils.java
+++ app/src/main/java/com/example/android/breakingnews/QueryUtils.java
@@ -115,11 +115,11 @@ public class QueryUtils {
                 String category = currentResults.getString("sectionName");
                 String date = currentResults.getString("webPublicationDate");
                 String url = currentResults.getString("webUrl");
-                JSONArray tagsauthor = currentResults.getJSONArray("references");
+                JSONArray tagsauthor = currentResults.getJSONArray("tags");
                 String author="";
                 if (tagsauthor.length()!= 0) {
                     JSONObject currenttagsauthor = tagsauthor.getJSONObject(0);
-                    author = currenttagsauthor.getString("author");
+                    author = currenttagsauthor.getString("webTitle");
                 }else{
                     author = "No Author ..";
                 }
